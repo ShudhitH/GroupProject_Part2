3653a92846642cc746c028f9ae873f07b4f118dd
shafi-3605 <105532757@student.swin.edu.au>
2025-05-24 18:47:50 +1000

    Update enhancement.php

--- enhancement.php
+++ enhancement.php
@@ -9,29 +9,22 @@
 <body>
   <?php include("header.inc"); ?>
   <?php include("nav.inc"); ?>
+
   <main>
     <h2>Enhancements</h2>
 
     <section>
       <h3>1. Modular Layout using PHP Includes</h3>
-      <p>We used PHP <code>include()</code> for header, nav, and footer to avoid repetition and ease updates.</p>
+      <p>We used PHP <code>include()</code> for header, navigation, and footer to reduce repetition and keep layout consistent across all pages.</p>
     </section>
 
     <section>
       <h3>2. “Other Skills” Validation</h3>
-      <p>If the “Other Skills” checkbox is selected, we ensure that the text input is not empty.</p>
+      <p>When the “Other Skills” checkbox is selected, users are required to enter text. This is enforced through both HTML and PHP validation.</p>
     </section>
-
-    <section>
-      <h3>3. Sortable EOI Records</h3>
-      <p>The HR manager can sort records by first name, job reference, or status using a dropdown menu.</p>
-    </section>
-
-    <section>
-      <h3>4. Login Lockout</h3>
-      <p>This is to improve the security, login access is locked after three failed attemptss.</p>
-    </section>
+    
   </main>
+
   <?php include("footer.inc"); ?>
 </body>
 </html>
